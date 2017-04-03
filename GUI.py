b25b43171193ee1ee603eb8a4c7f4df30284f6d2
Heinzza <Heinzza@users.noreply.github.com>
2017-04-03 12:15:32 +0300

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -2,6 +2,7 @@
 import sys
 from tkinter import *
 
+
 def lopeta():
     pohja.destroy()
     sys.exit(0)
@@ -32,14 +33,23 @@ pohja = Tk()
 pohja.title("Senior Notification Interface")
 pohja.configure(background='grey')
 
+pvm = Label(pohja,text="Päivämäärä tähän", font=("Arial",11))
+pvm.pack()
+pvm.place(relx=0.05, rely=0.04, anchor=S)
+pvm.config(background="grey")
 
-label = Label(pohja,text="HEI. ONKO KAIKKI HYVIN?")
+huone = Label(pohja, text="Huonenumero tähän", font=("Arial",11))
+huone.pack()
+huone.place(relx=0.055, rely=0.07, anchor=S)
+huone.config(background="grey")
+
+
+label = Label(pohja,text="HEI. ONKO KAIKKI HYVIN?",font=("Arial",28))
 label.pack()
 label.config(background = "grey")
-label.config(height=10,width=200) 
-label.configure(font="bold")
+label.place(relx=0.5, rely=0.15, anchor=CENTER)
+label.config(height=10,width=30) 
 
- 
 
 lopeta = Button(pohja, text = "QUIT", command=lopeta)
 lopeta.pack(side = BOTTOM)
@@ -48,58 +58,57 @@ lopeta.config(height=1,width=6)
 lopeta.config(font="bold")
 
 
-kutsuteksti = Label(pohja, text="Paina 1 lähettääksesi")
+kutsuteksti = Label(pohja, text="Paina 1 lähettääksesi",font=("Arial",13))
 kutsuteksti.pack()
 kutsuteksti.config(height=2,width=20)
 kutsuteksti.config(background = "grey")
-kutsuteksti.place(relx=0.35, rely=0.4, anchor=CENTER)
-kutsu = Button(pohja, text = "KUTSU", command=kutsu)
-kutsu.place(relx=0.35, rely=0.5, anchor=CENTER)
+kutsuteksti.place(relx=0.2, rely=0.4, anchor=CENTER)
+kutsu = Button(pohja, text = "KUTSU", font=("Arial",20), command=kutsu)
+kutsu.place(relx=0.2, rely=0.5, anchor=CENTER)
 pohja.bind("1", ku)
-kutsu.config( height = 6, width = 13)
+kutsu.config( height = 3, width = 9)
 kutsu.configure(background='green')
-kutsu.configure(font="bold")
 
 
-hätäteksti = Label(pohja, text="Paina 2 lähettääksesi")
+
+hätäteksti = Label(pohja, text="Paina 2 lähettääksesi",font=("Arial",13))
 hätäteksti.pack()
 hätäteksti.config(height=2,width=20)
 hätäteksti.config(background = "grey")
-hätäteksti.place(relx=0.45, rely=0.4, anchor=CENTER)
-hätä = Button(pohja, text = "HÄTÄ", command=hätä)
-hätä.place(relx=0.45, rely=0.5, anchor=CENTER)
+hätäteksti.place(relx=0.4, rely=0.4, anchor=CENTER)
+hätä = Button(pohja, text = "HÄTÄ", font=("Arial",20), command=hätä)
+hätä.place(relx=0.4, rely=0.5, anchor=CENTER)
 pohja.bind("2", hä)
-hätä.config( height = 6, width = 13)
+hätä.config( height = 3, width = 9)
 hätä.configure(background='red')
-hätä.configure(font="bold")
+
 
     
-kylläteksti = Label(pohja, text="Paina 3 vastataksesi")
+kylläteksti = Label(pohja, text="Paina 3 vastataksesi",font=("Arial",13))
 kylläteksti.pack()
 kylläteksti.config(height=2,width=20)
 kylläteksti.config(background = "grey")
-kylläteksti.place(relx=0.55, rely=0.4, anchor=CENTER)
-kyllä = Button(pohja, text = "KYLLÄ", command=kyllä)
-kyllä.place(relx=0.55, rely=0.5, anchor=CENTER)
+kylläteksti.place(relx=0.6, rely=0.4, anchor=CENTER)
+kyllä = Button(pohja, text = "KYLLÄ",font=("Arial",20),command=kyllä)
+kyllä.place(relx=0.6, rely=0.5, anchor=CENTER)
 pohja.bind("3", ky)
-kyllä.config( height = 6, width = 13)
+kyllä.config( height = 3, width = 9)
 kyllä.configure(background='green')
-kyllä.configure(font="bold")
 
 
-eiteksti = Label(pohja, text="Paina 4 vastataksesi")
+
+eiteksti = Label(pohja, text="Paina 4 vastataksesi",font=("Arial",13))
 eiteksti.pack()
 eiteksti.config(height=2,width=20)
 eiteksti.config(background = "grey")
-eiteksti.place(relx=0.65, rely=0.4, anchor=CENTER)
-ei = Button(pohja, text = "EI", command=ei)
-ei.place(relx=0.65, rely=0.5, anchor=CENTER)
+eiteksti.place(relx=0.8, rely=0.4, anchor=CENTER)
+ei = Button(pohja, text = "EI",font=("Arial",20), command=ei)
+ei.place(relx=0.8, rely=0.5, anchor=CENTER)
 pohja.bind("4", e)
-ei.config( height = 6, width = 13)
+ei.config( height = 3, width = 9)
 ei.configure(background='red')
-ei.configure(font="bold")
+
 
 
 
 pohja.mainloop()
-
