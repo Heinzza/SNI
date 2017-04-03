60588a5c456ce9c1b40585dae5d04877be46752f
Heinzza <Heinzza@users.noreply.github.com>
2017-04-03 10:12:32 +0300

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -27,41 +27,54 @@ def e(ei):
     print("En")
 
 
-
 pohja = tkinter.Tk()
 pohja.title("Senior Notification Interface")
 pohja.configure(background='grey')
 
+
 label = tkinter.Label(pohja,text="HEI. ONKO KAIKKI HYVIN?")
 label.pack()
+label.config(background = "grey")
+
+label.config(height=10,width=200) 
+label.configure(font="bold")
+ 
 
 lopeta = tkinter.Button(pohja, text = "QUIT", command=lopeta)
 lopeta.pack(side = BOTTOM)
+lopeta.configure(background='grey')
+
 
 kutsu = tkinter.Button(pohja, text = "KUTSU", command=kutsu)
 kutsu.place(relx=0.35, rely=0.5, anchor=CENTER)
 pohja.bind("1", ku)
-kutsu.config( height = 4, width = 12)
+kutsu.config( height = 6, width = 13)
 kutsu.configure(background='green')
+kutsu.configure(font="bold")
+
 
 hätä = tkinter.Button(pohja, text = "HÄTÄ", command=hätä)
 hätä.place(relx=0.45, rely=0.5, anchor=CENTER)
 pohja.bind("2", hä)
-hätä.config( height = 4, width = 12)
+hätä.config( height = 6, width = 13)
 hätä.configure(background='red')
+hätä.configure(font="bold")
 
     
 kyllä = tkinter.Button(pohja, text = "KYLLÄ", command=kyllä)
 kyllä.place(relx=0.55, rely=0.5, anchor=CENTER)
 pohja.bind("3", ky)
-kyllä.config( height = 4, width = 12)
+kyllä.config( height = 6, width = 13)
 kyllä.configure(background='green')
+kyllä.configure(font="bold")
+
 
 ei = tkinter.Button(pohja, text = "EI", command=ei)
 ei.place(relx=0.65, rely=0.5, anchor=CENTER)
 pohja.bind("4", e)
-ei.config( height = 4, width = 12)
+ei.config( height = 6, width = 13)
 ei.configure(background='red')
+ei.configure(font="bold")
 
 pohja.mainloop()
 
