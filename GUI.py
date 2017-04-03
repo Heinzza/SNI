a8dbe17c0171bfa6440b8c68d5c20364612a8c59
Heinzza <Heinzza@users.noreply.github.com>
2017-04-03 10:39:33 +0300

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -2,7 +2,6 @@ import tkinter
 from tkinter import *
 
 def lopeta():
-    import sys
     pohja.destroy()
     sys.exit(0)
 
@@ -35,9 +34,9 @@ pohja.configure(background='grey')
 label = tkinter.Label(pohja,text="HEI. ONKO KAIKKI HYVIN?")
 label.pack()
 label.config(background = "grey")
-
 label.config(height=10,width=200) 
 label.configure(font="bold")
+
  
 
 lopeta = tkinter.Button(pohja, text = "QUIT", command=lopeta)
@@ -45,6 +44,11 @@ lopeta.pack(side = BOTTOM)
 lopeta.configure(background='grey')
 
 
+kutsuteksti = tkinter.Label(pohja, text="Paina 1 lähettääksesi")
+kutsuteksti.pack()
+kutsuteksti.config(height=2,width=20)
+kutsuteksti.config(background = "grey")
+kutsuteksti.place(relx=0.35, rely=0.4, anchor=CENTER)
 kutsu = tkinter.Button(pohja, text = "KUTSU", command=kutsu)
 kutsu.place(relx=0.35, rely=0.5, anchor=CENTER)
 pohja.bind("1", ku)
@@ -53,6 +57,11 @@ kutsu.configure(background='green')
 kutsu.configure(font="bold")
 
 
+hätäteksti = tkinter.Label(pohja, text="Paina 2 lähettääksesi")
+hätäteksti.pack()
+hätäteksti.config(height=2,width=20)
+hätäteksti.config(background = "grey")
+hätäteksti.place(relx=0.45, rely=0.4, anchor=CENTER)
 hätä = tkinter.Button(pohja, text = "HÄTÄ", command=hätä)
 hätä.place(relx=0.45, rely=0.5, anchor=CENTER)
 pohja.bind("2", hä)
@@ -61,6 +70,11 @@ hätä.configure(background='red')
 hätä.configure(font="bold")
 
     
+kylläteksti = tkinter.Label(pohja, text="Paina 3 vastataksesi")
+kylläteksti.pack()
+kylläteksti.config(height=2,width=20)
+kylläteksti.config(background = "grey")
+kylläteksti.place(relx=0.55, rely=0.4, anchor=CENTER)
 kyllä = tkinter.Button(pohja, text = "KYLLÄ", command=kyllä)
 kyllä.place(relx=0.55, rely=0.5, anchor=CENTER)
 pohja.bind("3", ky)
@@ -69,12 +83,19 @@ kyllä.configure(background='green')
 kyllä.configure(font="bold")
 
 
+eiteksti = tkinter.Label(pohja, text="Paina 4 vastataksesi")
+eiteksti.pack()
+eiteksti.config(height=2,width=20)
+eiteksti.config(background = "grey")
+eiteksti.place(relx=0.65, rely=0.4, anchor=CENTER)
 ei = tkinter.Button(pohja, text = "EI", command=ei)
 ei.place(relx=0.65, rely=0.5, anchor=CENTER)
 pohja.bind("4", e)
 ei.config( height = 6, width = 13)
 ei.configure(background='red')
 ei.configure(font="bold")
+
+
 
 pohja.mainloop()
 
